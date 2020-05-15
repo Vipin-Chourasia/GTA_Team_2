45bbece0d61ff13d576f3126846c045719dee216
Vipin-Chourasia <Chourasia.vipin@gmail.com>
2020-05-15 13:04:20 -0500

    Initial Commit

---
+++ SourceCode/main.c
@@ -0,0 +1,19 @@
+#include "hello.h"
+#include <stdio.h>
+int add(int a, int b)
+{
+  return (a+b)
+}
+
+int subtract(int a, int b)
+{
+  return (a+b)
+}
+int main()
+{
+	int c,d;
+	c=add(1,2);
+	d=subtract(4,5);
+	printf("Add:%d\n",c);
+	printf("Subtract:%d\n",d);
+}
