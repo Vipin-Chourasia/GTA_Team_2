ed13945eee707a3489ce1b183e318141a4d9216f
Vipin-Chourasia <Chourasia.vipin@gmail.com>
2020-05-17 18:56:27 -0500

    Updated for Test Cases

--- SourceCode/Calculator.c
+++ SourceCode/Calculator.c
@@ -7,5 +7,5 @@ int add(int a, int b)
 
 int subtract(int a, int b)
 {
-  return (a+b);
+  return (a-b);
 }
--- Unit_Test.c
+++ Unit_Test.c
@@ -2,6 +2,9 @@
 #include <CUnit/Basic.h>
 #include "SourceCode/Calculator.h"
 
+int init_suite(void) { return 0; }
+int clean_suite(void) { return 0; }
+
 int maxi(int i1, int i2)
 {
     return (i1 > i2) ? i1 : i2;
@@ -22,18 +25,58 @@ void test_subtract(void)
     CU_ASSERT(subtract(1,2) == -1);
 }
 int main() {
-    CU_initialize_registry();
-    CU_pSuite suite_maxi = CU_add_suite("maxi_test", 0, 0);
-    CU_pSuite suite_add = CU_add_suite("add_test", 0, 0);
-    CU_pSuite suite_subtract = CU_add_suite("subtract_test", 0, 0);
+    // Initialize CUnit test registry
+    if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
 
-    CU_add_test(suite_maxi, "maxi_fun", test_maxi);
-    CU_add_test(suite_add, "add_fun", test_add);
-    CU_add_test(suite_subtract, "subtract_fun", test_subtract);
+    // Add suite_maxi to registry
+    CU_pSuite suite_maxi = CU_add_suite("maxi_test", init_suite, clean_suite);
+    if (NULL == suite_maxi) 
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
 
+    // add test_maxi to suite_maxi
+    if ((NULL == CCU_add_test(suite_maxi, "maxi_fun", test_maxi)))
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+    
+    
+    // Add suite_add to registry
+    CU_pSuite suite_add = CU_add_suite("add_test", init_suite, clean_suite);
+    if (NULL == suite_add) 
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // add test_add to suite_add
+    if ((NULL == CU_add_test(suite_add, "add_fun", test_add)))
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+    
+    // Add suite_subtract to registry
+    CU_pSuite suite_subtract = CU_add_suite("subtract_test", init_suite, clean_suite);
+    if (NULL == suite_subtract) 
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // add test_add to suite_add
+    if ((NULL == CU_add_test(suite_subtract, "subtract_fun", test_subtract)))
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+    
     CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
+    CU_basic_run_tests(); // OUTPUT to the screen
     CU_cleanup_registry();
-
-    return 0;
+    return CU_get_error();
 }
